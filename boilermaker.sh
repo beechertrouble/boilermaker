59d512b9a21ad3456d3225bd989a662c9f40245c
beecher <beechertrouble@gmail.com>
2015-09-02 20:58:23 -0400

    sh testing

--- boilermaker.sh
+++ boilermaker.sh
@@ -11,9 +11,9 @@ printf "\n\n"
 printf "${BLUE}-------------------------\n"
 printf "${BLUE}boilermaker : ${NORMAL}deleting the boilermaker repo ...\n"
 
-rm -rf .git
-rm -rf .gitignore
-mv _boilermaker-plates/.gitignore .gitignore
+#rm -rf .git
+#rm -rf .gitignore
+#mv _boilermaker-plates/.gitignore .gitignore
 if [ -d .git ]; then 
 	printf "${RED}error deleting git repo.\n"
 	printf "${NORMAL}try running with sudo?\n"
@@ -32,9 +32,29 @@ printf "\n\n"
 printf "${BLUE}-------------------------\n"
 printf "${BLUE}boilermaker : ${NORMAL}creating new git repo ...\n"
 
-git init
-git add .
-git commit -am 'boilermaker slammed! ( initial commit )'
+repo_name=$1
+dir_name=`basename $(pwd)`
+account_name=$2
+
+if [ "$repo_name" = "" ]; then
+	printf "Please enter your ACCOUNT_NAME and press enter. \n"
+	read account_name 
+	printf "Please enter the REPO_NAME and press enter. \n"
+	read repo_name
+fi
+
+if [ "$repo_name" = "" ]; then
+	repo_name=$dir_name
+fi
+
+printf "repo name = $repo_name\n"
+printf "account name = $account_name\n"
+
+# git init
+# git add .
+# git commit -am 'boilermaker slammed! ( initial commit )'
+# git remote add origin "https://github.com/$account_name/$repo_name.git"
+# git push origin master
 
 printf "${BLUE}-------------------------${NORMAL}"
 printf "\n\n"
@@ -57,9 +77,13 @@ printf "\n\n"
 printf "${BLUE}-------------------------\n"
 printf "${BLUE}boilermaker : ${NORMAL}remove the boilermaker plates ...\n"
 
-rm -rf _boilermaker-plates
-rm -rf boilermaker.sh 
+#rm -rf _boilermaker-plates
+#rm -rf boilermaker.sh 
 
-printf "\n${GREEN}DONE!\n\n"
-printf "${BLUE}-------------------------${NORMAL}"
-printf "\n\n\n\n"
+printf "\n${GREEN}\n"
+printf ">-------------------------\n"
+printf ">-------------------------\n\n"
+printf "DONE!\n\n"
+printf ">-------------------------\n"
+printf ">-------------------------"
+printf "${NORMAL}\n\n\n\n"
